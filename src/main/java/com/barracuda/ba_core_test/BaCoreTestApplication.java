292632555e97fe40b7922df7c27452c27a2c29f6
yydy1991 <151978839+yydy1991@users.noreply.github.com>
2024-10-25 20:45:43 +0800

    删除无用配置文件等

--- src/main/java/com/barracuda/ba_core_test/BaCoreTestApplication.java
+++ src/main/java/com/barracuda/ba_core_test/BaCoreTestApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = {"com.ruoyi", "com.barracuda"}, exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = {"com.barracuda"}, exclude = {DataSourceAutoConfiguration.class})
 public class BaCoreTestApplication {
 
     public static void main(String[] args) {
